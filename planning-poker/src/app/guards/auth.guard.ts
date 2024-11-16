4c37b13d77bb9559bc6e62e77dfc03519a75aa2b
Sam Daugherty <samdaugherty5@icloud.com>
2024-11-16 17:26:09 -0500

    Working version

--- planning-poker/src/app/guards/auth.guard.ts
+++ planning-poker/src/app/guards/auth.guard.ts
@@ -1,12 +1,20 @@
 import { inject } from '@angular/core';
 import { Router, type CanActivateFn } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { PokerHubConnectionService } from '../services/poker-hub-connection.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const userService = inject(UserService);
   const router = inject(Router);
+  const hubConnection = inject(PokerHubConnectionService);
 
   if (userService.isLoggedIn()) {
+    // join game
+    const currentUser = userService.getCurrentUser();
+    if (currentUser) {
+      console.log('Joining game...');
+      hubConnection.invoke('joinGame', currentUser.name, false);
+    }
     return true;
   }
 
